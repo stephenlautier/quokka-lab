5adb107ed755cd07247ea67b233fe3fc4acaa08e
Stephen Lautier <stephen.lautier@outlook.com>
2020-02-26 14:16:17 +0100

    add mock data

---
+++ src/mock-data.ts
@@ -0,0 +1,26 @@
+export const HEROES: Hero[] = [{
+	key: "rexxar",
+	name: "Rexxar",
+	role: RoleType.tank,
+	damage: 50,
+}, {
+	key: "jaina",
+	name: "Jaina",
+	role: RoleType.assassin,
+	damage: 150
+}, {
+	key: "valeera",
+	name: "Valeera",
+	role: RoleType.assassin,
+	damage: 140
+}, {
+	key: "sylvanas",
+	name: "Sylvanas",
+	role: RoleType.specialist,
+	damage: 90
+}, {
+	key: "malthael",
+	name: "Malthael",
+	role: RoleType.assassin,
+	damage: 160
+}];
